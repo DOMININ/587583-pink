884753159309ea77e361c10fd50ee862537483c5
Dmitry Dolgushin <razor9714@gmail.com>
2018-10-13 21:00:40 +0600

    added a tablet version of the forms page

--- source/js/menuToggle.js
+++ source/js/menuToggle.js
@@ -1,14 +1,18 @@
 var navMain = document.querySelector('.main-nav');
+var headerMain = document.querySelector('.page-header');
 var navToggle = document.querySelector('.main-nav__toggle');
 
 navMain.classList.remove('main-nav--nojs');
+headerMain.classList.remove('page-header--opened');
 
 navToggle.addEventListener('click', function() {
   if (navMain.classList.contains('main-nav--closed')) {
     navMain.classList.remove('main-nav--closed');
     navMain.classList.add('main-nav--opened');
+    headerMain.classList.add('page-header--opened');
   } else {
     navMain.classList.add('main-nav--closed');
     navMain.classList.remove('main-nav--opened');
+    headerMain.classList.remove('page-header--opened');
   }
 });
